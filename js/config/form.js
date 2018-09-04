147444fe8fce3a6a4eb666e6193d90d7cd06a170
Amenity Applewhite <amenity.applewhite@austintexas.gov>
2018-09-03 20:51:02 -0500

    Add fields to first page, 'Tell us how to contact you'

--- js/config/form.js
+++ js/config/form.js
@@ -1,4 +1,5 @@
 import Introduction from '../components/Introduction.jsx';
+import { phoneConfig } from 'us-forms-system/lib/js/definitions/phone';
 
 const formConfig = {
   title: 'Apply for a Residential Parking Permit',
@@ -16,23 +17,94 @@ const formConfig = {
       title: 'Contact Information',
       pages: {
         firstPage: {
-          path: 'contact/first-page',
-          title: 'First Page',
-          uiSchema: {},
+          path: 'contact/contact',
+          title: 'Contact Information',
           schema: {
             type: 'object',
-            properties: {}
-          }
+            properties: {
+              'view:textObject': {
+                type: 'object',
+                properties: {}
+              },
+              firstName: {
+                type: 'string'
+              },
+              lastName: {
+                type: 'string'
+              },
+              email: {
+                type: 'string',
+                format: 'email'
+              },
+              // TODO: Figure out why the phone definition is breaking stuff; see https://github.com/usds/us-forms-system/blob/44bf861a0fe641f7d566c70cffb5d3ffd44ba9af/docs/building-a-form/common-definitions.md#phone
+              //phone: phoneConfig.schema(), 
+              phone: {
+                type: 'string',
+                minLength: '10'
+              },
+              street: {
+                type: 'string'
+              },
+              city: {
+                type: 'string'
+              },
+              state: {
+                type: 'string',
+                enum: ['AK', 'IL', 'MA', 'TX', 'WY']
+              },
+              zip: {
+                type: 'string',
+                pattern: '^[0-9]{5}$'
+              }
+            }
+          },
+          uiSchema: {
+            'view:textObject': {
+              'ui:description': 'Tell us how to contact you.'
+            },
+            firstName: {
+              'ui:title': 'First name'
+            },
+            lastName: {
+              'ui:title': 'Last name'
+            },
+            email: {
+              'ui:title': 'Email address'
+            },
+           // phone: phoneConfig.uiSchema('Phone number'),
+            phone: {
+              'ui:title': 'Phone number',
+              'ui:description': 'Enter a 10 digit phone number',
+              'ui:errorMessages': {
+                'minLength': 'Please enter a valid 10 digit phone number'
+              }
+            },
+            street: {
+              'ui:title': 'Street'
+            },
+            city: {
+              'ui:title': 'City'
+            },
+            state: {
+              'ui:title': 'State',
+              'ui:options': {
+                labels: {
+                  AK: 'Alaska',
+                  IL: 'Illinois',
+                  MA: 'Massachusetts',
+                  TX: 'Texas',
+                  WY: 'Wyoming'
+                }
+              }
+            },
+            zip: {
+              'ui:title': 'ZIP code',
+              'ui:errorMessages': {
+                'pattern': 'Please enter a valid 5 digit US ZIP code.'
+              }
+            }
+          },
         },
-        secondPage: {
-          path: 'first-chapter/second-page',
-          title: 'Second Page',
-          uiSchema: {},
-          schema: {
-            type: 'object',
-            properties: {}
-          }
-        }
       }
     },
     secondChapter: {
